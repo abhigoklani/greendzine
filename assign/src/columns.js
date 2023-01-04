91697ee451da85fc9e1e8c91dd8e8cdca1f275c4
abhigoklani <abhishekgoklani0@gmail.com>
2023-01-04 12:31:13 +0530

    Initial commit

---
+++ assign/src/columns.js
@@ -0,0 +1,23 @@
+export const userColumns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id"
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "First Name",
+      dataIndex: "first_name",
+      key: "first_name"
+    },
+    {
+      title: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+  ];
+  
